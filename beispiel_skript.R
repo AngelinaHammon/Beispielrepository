9adf4944a8aa5a6f5d3a879927d1b6cc35f763c1
AngelinaHammon <46278452+AngelinaHammon@users.noreply.github.com>
2026-04-23 09:59:28 +0200

    Create beispiel_skript.R
    Added example R file with weird rounding in R

---
+++ beispiel_skript.R
@@ -0,0 +1,8 @@
+# Beispielskript
+pacman::p_load(magick, dplyr)
+# Beispielrechnung
+round(1.5) - round(2.5) 
+# Abhängig von geraden/ungeraden Zahlen
+round(1:10 + 0.5)
+
+image_read("surprised_pikachu.jpg") %>% print()
